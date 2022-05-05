7c60ed97c99a8326502e30524675c837ad9532a4
Stevens-97 <86322472+Stevens-97@users.noreply.github.com>
2022-05-05 12:29:11 +0100

    Added the API call and hooked this up to the search
    Added a form, handleClick, handleInput, useStates for input, toggle and city to search

--- my-app/src/Components/Input/index.js
+++ my-app/src/Components/Input/index.js
@@ -1,13 +1,57 @@
 import search from "../../search.svg";
 import "./index.css";
+import { useEffect, useState } from "react";
 
 function Input() {
+  const [toggleSearch, setToggleSearch] = useState(false);
+  const [inputText, setInputText] = useState("");
+  const [city, setCity] = useState("");
+  useEffect(() => {
+    async function fetchData() {
+      if (toggleSearch) {
+        let res = await fetch(
+          `http://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=a62faa97d1a6202196954776c4f104b3`
+        );
+        let data = await res.json();
+        console.log(data);
+        console.log(data.list[0]);
+        setToggleSearch(false);
+      }
+    }
+    fetchData();
+  }, [toggleSearch]);
+  function handleInput(e) {
+    console.log(e.target.value);
+    setInputText(e.target.value);
+  }
+
+  function handleClick(e) {
+    e.preventDefault();
+    setCity(inputText);
+    console.log(city);
+    setToggleSearch(true);
+  }
+
   return (
     <div>
       <form>
         <div class="searchBar">
-          <img class="searchIcon" src={search} alt="Search" />
-          <input type="search" id="search-input" />
+          <form>
+            <button
+              type="submit"
+              onClick={(e) => {
+                handleClick(e);
+              }}
+            ></button>
+            <img class="searchIcon" src={search} alt="Search" />
+            <input
+              type="search"
+              id="search-input"
+              onChange={(e) => {
+                handleInput(e);
+              }}
+            />
+          </form>
         </div>
       </form>
     </div>
