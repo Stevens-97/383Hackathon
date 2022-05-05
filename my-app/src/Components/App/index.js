dc73fd14b43d7621cf7bbb34949e3152c5946489
jafcoder <joefarr113@gmail.com>
2022-05-05 10:40:49 +0100

    changed file structure, made input component

---
+++ my-app/src/Components/App/index.js
@@ -0,0 +1,12 @@
+import "./index.css";
+import Input from "../Input";
+
+function App() {
+  return (
+    <div className="content">
+      <Input></Input>
+    </div>
+  );
+}
+
+export default App;
