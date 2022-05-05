a0875cf8724abd22fe9284b52337ff4162312bfd
jafcoder <joefarr113@gmail.com>
2022-05-05 10:57:20 +0100

    added search svg and made input styled

--- my-app/src/Components/Input/index.js
+++ my-app/src/Components/Input/index.js
@@ -1,7 +1,15 @@
+import search from "../../search.svg";
+import "./index.css";
+
 function Input() {
   return (
     <div>
-      <input></input>
+      <form>
+        <div class="searchBar">
+          <img class="searchIcon" src={search} alt="Search" />
+          <input type="search" id="search-input" />
+        </div>
+      </form>
     </div>
   );
 }
