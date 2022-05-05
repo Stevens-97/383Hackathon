9698fd12900dc249d4be055cdaaee08461671b7e
jafcoder <joefarr113@gmail.com>
2022-05-05 14:46:27 +0100

    added styling and started mobile responsiveness, card components change size on tablet and smaller

--- my-app/src/Components/App/index.js
+++ my-app/src/Components/App/index.js
@@ -4,15 +4,16 @@ import InitialForecast from "../InitialForcast";
 
 import { useState } from "react";
 
-
 function App() {
   const [data, setData] = useState([]);
   const [city, setCity] = useState("");
 
   return (
     <div className="content">
+      <h1>Weather App</h1>
       <Input setData={setData} setCity={setCity} city={city} />
-      <div>
+      <h2 className="cityName">{city}</h2>
+      <div className="forecastDisplay">
         {data.map((dailyForecast, index) => (
           <InitialForecast
             city={city}
